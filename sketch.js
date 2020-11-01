f8fe271686f4259571a3b498c3e1da5676e2f69b
taran004 <71597941+taran004@users.noreply.github.com>
2020-11-01 20:09:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,14 +5,14 @@ var speed;
 var weight;
 
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(1200,400);
   speed = random(55,90);
-  weight = random(400,1500);
+  weight = random(400,500);
   car = createSprite(50,200,50,50); 
- wall = createSprite(1500,200,60,height/2)
+ wall = createSprite(1100,200,60,height/2)
  wall.shapeColor = color(80,80,80);
- car.velocityX = speed;
- Deformation = 0.5*weight*speed*speed/2500
+ car.velocityX = speed
+ deformation = 0.5*weight*speed*speed/22500
 }
 
 function draw() {
@@ -20,7 +20,7 @@ function draw() {
 
 if(wall.x - car.x < (car.width + wall.width)/2){
 car.velocityX = 0;
-var deformation = 0.5*weight*speed*speed/22509;
+
 if(deformation>180){
  car.shapeColor = color(255,0,0);
 }
@@ -30,7 +30,7 @@ if(deformation<180 && deformation>100){
 if(deformation<100){
 car.shapeColor = color(0,255,0);
 }
-
+text(deformation,100,100);
 }
   drawSprites();
 }
